608fd5915d0127dc5e29fcb997c09ad1027556a1
atulgawali <atul.gawali@kratin.co.in>
2017-02-19 16:42:14 +0530

    first commit

---
+++ MediaPicker/Helper/DocDirectoryMaanger.h
@@ -0,0 +1,13 @@
+//
+//  DocDirectoryMaanger.h
+//  MediaPicker
+//
+//  Created by Atul on 17/02/17.
+//  Copyright © 2017 Atul Gawali. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DocDirectoryMaanger : NSObject
+
+@end
---
+++ MediaPicker/Helper/DocDirectoryMaanger.m
@@ -0,0 +1,13 @@
+//
+//  DocDirectoryMaanger.m
+//  MediaPicker
+//
+//  Created by Atul on 17/02/17.
+//  Copyright © 2017 Atul Gawali. All rights reserved.
+//
+
+#import "DocDirectoryMaanger.h"
+
+@implementation DocDirectoryMaanger
+
+@end
